9dba5ecaaa75bef6a4a153bcabe1d77a44d43344
giangkhongaa <trantruonggiang04dhth3.hufi@gmail.com>
2017-01-16 01:18:08 +0700

    Edit

--- Spline/js/data.js
+++ Spline/js/data.js
@@ -1,16 +1,17 @@
-var data = {  title: 'BIỂU ĐỒ LỊCH SỬ ĐÁNH GIÁ 360',
-			  titleFont: '20pt Myriad Pro',
-			  titleStyle: 'red',
-              xLabel: 'Tên dự án', 
-              yLabel: 'Điểm đánh giá 360',
-              labelFont: 'italic 12pt Adobe Ming Std L',
-			  labelStyle: '#000000',
-			  splineStyle: '#019ed5',
-			  dataPoints:[{x: 'A',   y: 2.3 },
-						  {x: 'B',   y: 1 }, 
-                          {x: 'C',   y: 3.5 },
-						  {x: 'D',   y: 4.1 },
-                          {x: 'E',   y: 3   },
-						  {x: 'F',   y: 4	},
-						  {x: 'F',   y: 2	}]
- };
+var data = {
+    title: 'BIỂU ĐỒ LỊCH SỬ ĐÁNH GIÁ 360',
+    titleFont: '20pt Myriad Pro',
+    titleStyle: 'red',
+    xLabel: 'Tên dự án',
+    yLabel: 'Điểm đánh giá 360',
+    labelFont: 'italic 12pt Adobe Ming Std L',
+    labelStyle: '#000000',
+    splineStyle: '#019ed5',
+    dataPoints: [{ x: 'A', y: 2.3 },
+                { x: 'B', y: 1 },
+                { x: 'C', y: 3.5 },
+                { x: 'D', y: 4.1 },
+                { x: 'E', y: 3 },
+                { x: 'F', y: 4 },
+                { x: 'F', y: 2 }]
+};
